e681c680adc03d7542d18f77c1cbb4aa9651649e
happy123903 <happy3635312@gmail.com>
2023-04-26 12:05:35 +0800

    Week11做更新

---
+++ Week11-3_PlaySound/Week11-3_PlaySound.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int main()
+{                                                                     ///µ¥«Ý¦P¨B
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+}
