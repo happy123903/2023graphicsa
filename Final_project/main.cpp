f80deea52b5f31781af13c54f009731711215944
happy123903 <happy3635312@gmail.com>
2023-05-18 00:03:24 +0800

    第十四周的OpenGL和GLUT專案

--- Final_project/main.cpp
+++ Final_project/main.cpp
@@ -5,19 +5,24 @@ GLMmodel * head = NULL;
 GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
-int show[4]={0,1,0,0};
-float angle=0, angle2=0, angle3=0;
+int show[4]={0,0,1,1};
+int ID=0;
 void keyboard(unsigned char key, int x , int y)
 {
-    if(key=='0') show[0]=!show[0];
+    if(key=='0') ID=0;
+    if(key=='1') ID=1;
+    if(key=='2') ID=2;
+    if(key=='3') ID=3;
+    /*if(key=='0') show[0]=!show[0];
     if(key=='1') show[1]=!show[1];
     if(key=='2') show[2]=!show[2];
-    if(key=='3') show[3]=!show[3];
+    if(key=='3') show[3]=!show[3];*/
     glutPostRedisplay();
 }
 FILE * fout = NULL;
 FILE * fin = NULL;
 float teapotX=0, teapotY=0;
+float angle=0, angle2=0,angle3=0;
 void display()
 {
 
@@ -31,15 +36,38 @@ void display()
             uparmR=glmReadOBJ("model/uparmR.obj");
             lowarmR=glmReadOBJ("model/lowarmR.obj");
         }
-        if(show[0]) glmDraw(head,GLM_MATERIAL);
-        if(show[1]) glmDraw(body,GLM_MATERIAL);
-        glPushMatrix();
-                glTranslatef(teapotX,teapotY,0);
-                if(show[2]) glmDraw(uparmR,GLM_MATERIAL);
-        glPopMatrix();
-        if(show[3]) glmDraw(lowarmR,GLM_MATERIAL);
-    glPopMatrix();
+        if(ID==0) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
+        if(show[0]) glmDraw(head , GLM_MATERIAL);
 
+        if(ID==1) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
+        if(show[1]) glmDraw(body , GLM_MATERIAL);
+
+        glPushMatrix();
+            glTranslatef(-1.260000, 0.440000, 0);
+            glRotatef(angle,0,0,1);
+            glTranslatef(1.260000, -0.440000, 0);
+            ///glTranslatef(teapotX,teapotY,0);
+
+            if(ID==2) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
+            if(show[2]) glmDraw(uparmR , GLM_MATERIAL);
+        glPopMatrix();
+
+        glPushMatrix();
+            glTranslatef(-1.193333, 0.600000, 0);
+            glRotatef(angle,0,0,1);
+            glTranslatef(1.193333, -0.600000, 0);
+            ///glTranslatef(teapotX,teapotY,0);
+
+            if(ID==3) glColor3f(1,0,0);
+            else glColor3f(1,1,1);
+            if(show[3]) glmDraw(lowarmR , GLM_MATERIAL);
+        glPopMatrix();
+    glPopMatrix();
+    glColor3f(0,1,0);///中心點綠茶壺
+    glutSolidTeapot(0.02);
     glutSwapBuffers();
 
 }
@@ -50,7 +78,8 @@ void motion(int x, int y)
     teapotY-=(y-oldY)/150.0;
     oldX=x;
     oldY=y;
-    printf("glTranslatef(%f, %f, 0\n",teapotX,teapotY);
+    angle=x;
+    printf("glTranslatef(%f, %f, 0)\n",teapotX,teapotY);
     glutPostRedisplay();
 }
 void mouse(int button, int state, int x, int y)
@@ -58,7 +87,6 @@ void mouse(int button, int state, int x, int y)
     if(state==GLUT_DOWN){
         oldX=x;
         oldY=y;
-        angle=x;
     }
     display();
 }
@@ -79,7 +107,7 @@ int main(int argc, char *argv[])//main()主函式 進階版
 
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);//雙緩衝區+3D深度功能
 
-    glutCreateWindow("week13");//開GLUT視窗
+    glutCreateWindow("week14");//開GLUT視窗
 
 
 
